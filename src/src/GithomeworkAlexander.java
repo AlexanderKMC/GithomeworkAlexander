30caf9c78cc76a79f707161a3456518b7a6b3d1e
AlexanderKMC <Smj500hsams@gmail.com>
2023-10-10 20:57:21 +0300

    ALEXANDER MIHLYK second commit

--- src/src/GithomeworkAlexander.java
+++ src/src/GithomeworkAlexander.java
@@ -1,6 +1,7 @@
 public class GithomeworkAlexander {
     public static void main(String[] args){
         System.out.println("My homework in git ALEXANDER");
+        System.out.println("second commit on GIT FROM ALEXANDER MIHLYK");
 
     }
 }
